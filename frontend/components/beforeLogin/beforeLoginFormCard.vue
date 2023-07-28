878797d06cc4224191dc4775b14420c9e332e98c
bduha184 <bduha_184@yahoo.co.jp>
2023-07-28 14:17:49 +0900

    新規登録・ログインのタブ切り替え実装

--- frontend/components/beforeLogin/beforeLoginFormCard.vue
+++ frontend/components/beforeLogin/beforeLoginFormCard.vue
@@ -1,61 +1,78 @@
 <template>
   <div class="p-4">
     <v-container class="bg-white">
-      <v-row class="text-center mb-4 relative before:absolute before:w-full before:h-[3px] before:bg-slate-300 before:left-0 before:bottom-0">
-        <v-col class="relative before:absolute before:w-full before:h-[3px] before:bg-orange-500 before:left-0 before:bottom-0">
+      <v-tabs
+        v-model="tab"
+        fixed-tabs
+        class="text-center mb-4"
+        slider-color="red"
+      >
+        <v-tab :value="register">
           <Icon :name="Icons.REGISTER" />
-          <v-list-item-title class="mt-4"> 新規登録 </v-list-item-title>
-        </v-col>
-        <v-col
-          ><Icon :name="Icons.LOGIN" />
-          <v-list-item-title class="mt-4"> ログイン </v-list-item-title></v-col
-        >
-      </v-row>
-
-      <v-row>
-        <v-col>
-          <v-btn
-            variant="outlined"
-            rounded="0"
-            class="relative d-block mx-auto border-2 border-indigo-500 w-[250px]"
-          >
-            <v-img
-              contain
-              src="../../assets/images/google.png"
-              class="mr-auto absolute"
-              max-width="20"
+          新規登録
+        </v-tab>
+        <v-tab :value="login">
+          <Icon :name="Icons.LOGIN" />
+          ログイン
+        </v-tab>
+      </v-tabs>
+      <v-window v-model="tab">
+        <v-window-item :value="register">
+          <v-row>
+            <v-col>
+              <v-btn
+                variant="outlined"
+                rounded="0"
+                class="relative d-block mx-auto border-2 border-indigo-500 w-[250px]"
+              >
+                <v-img
+                  contain
+                  src="../../assets/images/google.png"
+                  class="mr-auto absolute"
+                  max-width="20"
+                />
+                Googleで登録
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-btn
+                variant="outlined"
+                rounded="0"
+                class="d-block mx-auto border-2 border-indigo-500 w-[250px]"
+              >
+                <v-img
+                  contain
+                  src="../../assets/images/twitter.png"
+                  class="mr-auto"
+                  max-width="20"
+                />
+                Twitterで登録
+              </v-btn>
+            </v-col>
+          </v-row>
+          <div class="supplement text-center my-4">
+            <p class="d-inline bg-white">またはメールアドレスで登録</p>
+          </div>
+          <Form>
+            <div class="text-caption">
+              <span class="text-red">※</span>は必須項目です
+            </div>
+            <slot name="form-card-content" />
+            <Btn
+              btnValue="登録"
+              place="MAIN"
+              width="16rem"
+              setColor="orange"
+              class="mt-4 d-block"
             />
-            Googleで登録
-          </v-btn>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-btn
-            variant="outlined"
-            rounded="0"
-            class="d-block mx-auto border-2 border-indigo-500 w-[250px]"
-          >
-            <v-img
-              contain
-              src="../../assets/images/twitter.png"
-              class="mr-auto"
-              max-width="20"
-            />
-            Twitterで登録
-          </v-btn>
-        </v-col>
-      </v-row>
-      <div class="supplement text-center my-4">
-        <p class="d-inline bg-white">またはメールアドレスで登録</p>
-      </div>
-      <Form>
-        <div class="text-caption">
-          <span class="text-red">※</span>は必須項目です
-        </div>
-        <slot name="form-card-content" />
-        <Btn btnValue="登録" place="MAIN" width="16rem" setColor="orange" class="mt-4 d-block" />
-      </Form>
+          </Form>
+        </v-window-item>
+        <v-window-item :value="login">
+          <h2>test</h2>
+        </v-window-item>
+      </v-window>
     </v-container>
   </div>
 </template>
@@ -63,6 +80,9 @@
 import { Icons } from "@/constants/icons";
 import { Form } from "vee-validate";
 import * as Yup from "yup";
+import { ref } from "vue";
+
+const tab = ref(null);
 </script>
 
 <style setup lang="scss">
