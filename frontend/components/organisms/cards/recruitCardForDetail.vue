0b1110c4f57e79294af36766a4138bbbdd343856
bduha184 <bduha_184@yahoo.co.jp>
2023-08-28 14:46:14 +0900

    チーム募集詳細の質問、参加ボタン設置 未ログイン時のモーダル設置

--- frontend/components/organisms/cards/recruitCardForDetail.vue
+++ frontend/components/organisms/cards/recruitCardForDetail.vue
@@ -1,5 +1,5 @@
 <template>
-   <v-card>
+  <v-card>
     <div class="relative z-0 h-[100px]">
       <AtomsImgsCardHeaderImg
         :disabled="false"
@@ -11,78 +11,85 @@
         />
       </AtomsImgsCardHeaderImg>
     </div>
-   <v-card-title class="text-body-2 pl-20">
-     <AtomsDecorationHeadline>
-      {{ recruit.getRecruitTitle }}
-     </AtomsDecorationHeadline>
-   </v-card-title>
-   <v-card-text>
+    <v-card-title class="text-body-2 pl-20">
+      <AtomsDecorationHeadline>
+        {{ recruit.getRecruitTitle }}
+      </AtomsDecorationHeadline>
+    </v-card-title>
+    <v-card-text>
       {{ recruit.getRecruitText }}
     </v-card-text>
-   <GalleryLightGallery/>
-   <TeamActivity/>
-   <v-container class="text-center">
-    <v-row>
-      <v-col>
-        <AtomsBtnsBaseBtn
-        @click="requestJoinTeam"
-        >
-         このチームに参加する
-       </AtomsBtnsBaseBtn>
-       <MoleculesAccordionsMessage
-       class="text-center"
-       :toggle="toggleRequest"
-       placeholder="伝えたい内容、参加したい理由、等を記載してください"
-       text="メッセージを送信する"
-       >
-       </MoleculesAccordionsMessage>
-      </v-col>
-      <v-col>
-        <AtomsBtnsBaseBtn
-        @click="questionToTeam"
-        setColor="orange">
-         このチームに質問する
-       </AtomsBtnsBaseBtn>
-       <MoleculesAccordionsMessage
-       class="text-center"
-       :setColor="orange"
-       :toggle="toggleQuestion"
-       placeholder="質問内容を記載してください"
-       text="質問内容を送信する"
-       >
-       </MoleculesAccordionsMessage>
-      </v-col>
-    </v-row>
-   </v-container>
-   <RecruitProfile
-   :prof_thumbnail_path="prof_thumbnail_path"
-   />
+    <GalleryLightGallery />
+    <TeamActivity />
+    <v-container class="text-center">
+      <v-row>
+        <v-col>
+          <MoleculesBeforeLoginModal
+          @emitClick="requestJoinTeam"
+          :toggle="toggleRequest"
+          >
+        このチームに参加する
+        </MoleculesBeforeLoginModal>
+          <MoleculesAccordionsMessage
+            class="text-center"
+            :toggle="toggleRequest"
+            placeholder="伝えたい内容、参加したい理由、等を記載してください"
+            text="メッセージを送信する"
+          >
+          </MoleculesAccordionsMessage>
+        </v-col>
+        <v-col>
+          <MoleculesBeforeLoginModal
+          @emitClick="questionToTeam"
+          :toggle="toggleQuestion"
+          setColor="orange"
+          >このチームに質問する</MoleculesBeforeLoginModal>
+          <MoleculesAccordionsMessage
+            class="text-center"
+            setColor="orange"
+            :toggle="toggleQuestion"
+            placeholder="質問内容を記載してください"
+            text="質問内容を送信する"
+          >
+          </MoleculesAccordionsMessage>
+        </v-col>
+      </v-row>
+    </v-container>
+    <RecruitProfile :prof_thumbnail_path="prof_thumbnail_path" />
   </v-card>
 </template>
 
 <script setup lang="ts">
-import {ref,onMounted} from 'vue';
-import { useRecruitStore } from '../../../stores/useRecruitStore';
-import {useRoute} from 'vue-router';
+import { useAuthStore } from "../../../stores/useAuthStore";
+import { ref, onMounted } from "vue";
+import { useRecruitStore } from "../../../stores/useRecruitStore";
+import { useRoute } from "vue-router";
 
+const auth = useAuthStore();
 const router = useRoute();
 const recruit = useRecruitStore();
 onMounted(() => {
   const itemId = router.params.id;
   recruit.fetchRecruitItem(itemId);
-})
+});
 
 const toggleRequest = ref(false);
 const toggleQuestion = ref(false);
 
 const requestJoinTeam = () => {
-  toggleRequest.value = !toggleRequest.value;
-}
+  if (!auth.isLoggedIn) {
+    toggleRequest.value = false;
+  } else {
+    toggleRequest.value = !toggleRequest.value;
+  }
+};
 const questionToTeam = () => {
-  toggleQuestion.value = !toggleQuestion.value;
-}
-
-
+  if (!auth.isLoggedIn) {
+    toggleQuestion.value = false;
+  } else {
+    toggleQuestion.value = !toggleQuestion.value;
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -91,15 +98,14 @@ const questionToTeam = () => {
     margin-top: 1rem;
   }
 }
-  .v-avatar {
-    position:absolute !important;
-    bottom:-40px;
-    left:20px;
-    z-index: 10;
-  }
-
-  .v-responsive {
-  position: unset !important;
+.v-avatar {
+  position: absolute !important;
+  bottom: -40px;
+  left: 20px;
+  z-index: 10;
 }
 
+.v-responsive {
+  position: unset !important;
+}
 </style>
